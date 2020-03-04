ca230e38b6d3fdaea6498809fed5a97dff70e572
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-03-04 15:02:49 +0000

    added placeholder

--- src/App.js
+++ src/App.js
@@ -7,7 +7,15 @@ import Form from "./components/Form"
 import Weather from "./components/Weather"
 
 const API_KEY = "224566a483405adb54b0ecf2dbbce14a"
+
+
 class App extends React.Component{
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=
+    ${API_KEY}&units=metric`);
+  const data = await api_call.json(); 
+  }
+  
   render(){
     return(
       <div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,11 @@ import React from 'react';
 class Form extends React.Component{
     render(){
         return(
-            <div>Form Component</div>
+            <form>
+                <input type="text" name="city" placeholder="city..."/>
+                
+                <input type="text" name="country" placeholder="country..."/>
+            </form>
         );
     }
 
